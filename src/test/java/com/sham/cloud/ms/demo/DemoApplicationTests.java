cb462b4634580d9b6b214c5bea138f0985164065
Sham Fiorin <sham.vinicius@gmail.com>
2020-02-26 00:31:55 -0300

    initial commit

---
+++ src/test/java/com/sham/cloud/ms/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sham.cloud.ms.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
